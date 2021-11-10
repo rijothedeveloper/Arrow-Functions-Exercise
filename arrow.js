82f82d38eada6e71ac37fafeb05be45b0fb54858
Rijo George <rijogeorge@macbook-pro-2.lan>
2021-11-10 11:02:44 -0800

    done exercise

---
+++ arrow.js
@@ -0,0 +1,29 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+  /* Write an ES2015 Version */
+
+  const double = arr => arr.map(val => val * 2)
+
+  function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+const squareAndFindEvens = numbers => {
+    const squares = numbers.map(num => num ** 2);
+    const evans = squares.filter( square => square % 2 === 0);
+    return evans;
+}
+
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter( square => square % 2 === 0);
+
+  
